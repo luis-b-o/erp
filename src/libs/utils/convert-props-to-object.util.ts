f70e1456fb251c72ee00d2846b53246486d91da2
Luis Borochok <luiseduardobo2011@gmail.com>
2024-07-12 09:22:31 -0300

    feat: entity and value-object bases

---
+++ src/libs/utils/convert-props-to-object.util.ts
@@ -0,0 +1,40 @@
+import { Entity, ValueObject } from '@ddd';
+
+function isEntity(obj: unknown): obj is Entity<unknown> {
+  return (
+    Object.prototype.hasOwnProperty.call(obj, 'toObject') &&
+    Object.prototype.hasOwnProperty.call(obj, 'id') &&
+    ValueObject.isValueObject((obj as Entity<unknown>).id)
+  );
+}
+
+function convertToPlainObject(item: any): any {
+  if (ValueObject.isValueObject(item)) {
+    return item.unpack();
+  }
+
+  if (isEntity(item)) {
+    return item.toObject();
+  }
+  return item;
+}
+
+/**
+ * Converts Entity/Value Objects props to a plain object.
+ * Useful for testing and debugging.
+ * @param props
+ */
+export function convertPropsToObject(props: any): any {
+  const propsCopy = structuredClone(props);
+
+  for (const prop in propsCopy) {
+    if (Array.isArray(propsCopy[prop])) {
+      propsCopy[prop] = (propsCopy[prop] as Array<unknown>).map((item) => {
+        return convertToPlainObject(item);
+      });
+    }
+    propsCopy[prop] = convertToPlainObject(propsCopy[prop]);
+  }
+
+  return propsCopy;
+}
